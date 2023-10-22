9990a567e71f0eedeae054b3a11c12059c88c8f6
Patrick Rengifo <patrick.rengifo@gmail.com>
2023-10-22 20:40:23 +0200

    Updated name of package

--- Package.swift
+++ Package.swift
@@ -9,15 +9,15 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "EComIDIntegrationsHM",
-            targets: ["EComIDIntegrationsHM"]),
+            name: "EComIDIntegrationsOverlay",
+            targets: ["EComIDIntegrationsOverlay"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
-            name: "EComIDIntegrationsHM",
-            path: "./Sources/EComIDIntegrationsHM.xcframework"
+            name: "EComIDIntegrationsOverlay",
+            path: "./Sources/EComIDIntegrationsOverlay.xcframework"
         )
     ]
 )
